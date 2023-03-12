8be01dc2a1bbc8a90878d5d8fadfe5993824f0ac
Elad Levy <eladlevysignups@gmail.com>
2023-03-12 17:35:56 +0200

    refactored animal class out of the table class

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 import enum
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-from typing import Dict
+from typing import Dict, Union, List
 from collections import defaultdict
 import requests
+from copy import copy
 
 class Column(enum.Enum):
     ANIMAL= 0
     COLLATERAL_ADJ= 5
+
+class Animal:
+    def __init__(self, row: Tag):
+        if not Animal.is_row_an_animal(row):
+            raise ValueError("Row does not represent an animal")
+
+        details = Animal.extract_details_from_row(row)
+        unprocessed_collaterals = details[Column.COLLATERAL_ADJ.value].get_text(strip=True,
+                                                                                     separator='\n').splitlines()
+        self.collateral_adjectives = Animal.process_collateral_adjectives(unprocessed_collaterals)
+        unprocessed_name = details[Column.ANIMAL.value].text
+        self.name = Animal.process_name(unprocessed_name)
+
+    @staticmethod
+    def extract_details_from_row(row: Tag) -> Union[list, None]:
+        animal_details = row.select("td")
+        if len(animal_details) > 1:  # rows representing animals have multiple details
+            return animal_details
+
+    @staticmethod
+    def is_row_an_animal(row: Tag) -> bool:
+        return bool(Animal.extract_details_from_row(row))
+
+    @staticmethod
+    def process_name(name: str) -> str:
+        exit_phrases = ["(", "[", " also see", " see"]
+        stripped_name = copy(name).capitalize()
+        for exit_phrase in exit_phrases:
+            stripped_name = stripped_name.split(exit_phrase)[0]
+        return stripped_name.strip()
+
+    @staticmethod
+    def process_collateral_adjectives(adjectives: List[str]) -> List[str]:
+        return [adj.capitalize().strip() for adj in adjectives if adj.isalpha()]  # screen redundant comments and marks
 
 class AnimalsTable:
     def __init__(self):
@@ -25,15 +60,10 @@ class AnimalsTable:
     def parse_html_table(table_element: Tag) -> Dict[str, list]:
         mapping = defaultdict(list)
         for row in table_element.select("tr"):
-            animal_details = row.select("td")
-            if len(animal_details) <= 1:
-                continue  # must be a letter seperator or header row
-            collateral_adjectives = animal_details[Column.COLLATERAL_ADJ.value].get_text(strip=True, separator='\n').splitlines()
-            collateral_adjectives = [adj.capitalize().strip() for adj in collateral_adjectives if adj.isalpha()]  # screen redundant comments and marks
-            animal_name = animal_details[Column.ANIMAL.value].text
-            animal_name = animal_name.capitalize().split("(")[0].split("[")[0].split(" also see")[0].split(" see")[0].strip()
-            for adjective in collateral_adjectives:
-                mapping[adjective].append(animal_name)
+            if Animal.is_row_an_animal(row):
+                animal = Animal(row)
+                for adjective in animal.collateral_adjectives:
+                    mapping[adjective].append(animal.name)
         return mapping
 
     def __str__(self):
